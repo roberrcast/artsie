c3eb13a080cd931e74f68a4f438862ea91217860
robercast <dunamis1.16@gmail.com>
2026-05-13 03:36:20 -0700

    Add imports to exhibitons details page

---
+++ src/__tests__/pages/ExhibitionsDetails.test.tsx
@@ -0,0 +1,3 @@
+import { describe, it, expect, vi } from "vitest";
+import { renderWithProviders, screen, fireEvent } from "../test-utils";
+import ExhibitionDetails from "../../pages/ExhibitionDetails";
